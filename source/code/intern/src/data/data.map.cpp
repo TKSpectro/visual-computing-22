eefae9f45877d2d94d708be722102cf01dda697a
TKSpectro <tomkaeppler@web.de>
2023-02-25 06:53:19 -0800

    add data.map

--- source/code/intern/src/data/data.map.cpp
+++ source/code/intern/src/data/data.map.cpp
@@ -2,41 +2,125 @@
 
 namespace Data
 {
-    EntityIterator Map::BeginEntity(Core::AABB2Float& AABB, EntityCategory::Enum category)
+    // Example for iterating over the whole map
+    void all()
     {
-        // Get first sector within AABB which contains an entity within the entity list of the given category
-        // return iterator to this entity, otherwise return end iterator
+        Map map;
 
+        const EntityIterator endIterator = map.End();
 
-        return EndEntity();
+        for (EntityIterator it = map.Begin(); it != endIterator; it.Next())
+        {
+            // it->CallSomeMethod
+
+            Entity& entity = *it;
+
+            // entity.CallSomeMethod
+        }
     }
 
-    // -----------------------------------------------------------------------------
-
-    EntityIterator Map::NextEntity(EntityIterator where, Core::AABB2Float& AABB, EntityCategory::Enum category)
+    // Example for iterating over specific area
+    void area()
     {
-        return EndEntity();
+        Core::AABB2Float someAABB;
+        Map map;
+
+        const EntityIterator endIterator = map.End();
+
+        for (EntityIterator it = map.Begin(someAABB); it != endIterator; it.Next(someAABB))
+        {
+            // it->CallSomeMethod
+
+            Entity& entity = *it;
+
+            // entity.CallSomeMethod
+        }
     }
 
-    // -----------------------------------------------------------------------------
 
-    EntityIterator Map::BeginEntity(Core::AABB2Float& AABB)
+
+    //EntityIterator Map::BeginEntity(Core::AABB2Float& AABB, EntityCategory::Enum category)
+    //{
+    //    // Get first sector within AABB which contains an entity within the entity list of the given category
+    //    // return iterator to this entity, otherwise return end iterator
+
+
+    //    return EndEntity();
+    //}
+
+    //// -----------------------------------------------------------------------------
+
+    //EntityIterator Map::NextEntity(EntityIterator where, Core::AABB2Float& AABB, EntityCategory::Enum category)
+    //{
+    //    return EndEntity();
+    //}
+
+    //// -----------------------------------------------------------------------------
+
+    //EntityIterator Map::BeginEntity(Core::AABB2Float& AABB)
+    //{
+    //    return EndEntity();
+    //}
+
+    //// -----------------------------------------------------------------------------
+
+    //EntityIterator Map::NextEntity(EntityIterator where, Core::AABB2Float& AABB)
+    //{
+    //    return EndEntity();
+    //}
+
+    //// -----------------------------------------------------------------------------
+
+    EntityIterator Map::Begin()
     {
-        return EndEntity();
-    }
 
-    // -----------------------------------------------------------------------------
 
-    EntityIterator Map::NextEntity(EntityIterator where, Core::AABB2Float& AABB)
-    {
-        return EndEntity();
-    }
-
-    // -----------------------------------------------------------------------------
-
-    EntityIterator Map::EndEntity()
-    {
         return EntityIterator();
     }
+
+    EntityIterator Map::Begin(Core::AABB2Float& aabb)
+    {
+        // Clamp this to int from float
+        int sectorX = aabb.GetMin()[0] / 16;
+        int sectorY = aabb.GetMin()[1] / 16;
+        if (aabb.GetMin()[0] < 1)
+        {
+            --sectorX;
+        }
+        if (aabb.GetMin()[1] < 1)
+        {
+            --sectorY;
+        }
+        if (sectorX < 0)
+        {
+            sectorX = 0;
+        }
+        if (sectorY < 0)
+        {
+            sectorY = 0;
+        }
+
+        Sector sector = sectors[sectorY * 8 + sectorX];
+        // ueber alle EntityFolder iterieren
+        //   if list.notEmpty()
+        //     for alle entitaeten in der liste
+        //       if entity AABB intersects inputAABB
+        //         return entityIterator;
+
+        // keine enity in dem Sektor gefunden
+        // sektor x++
+        // if (sektor x++ > maxX)
+        //   sektorY++
+        //   sektorX = sectorXMin vom Anfang
+        //   If( sectorY > maxY)
+        //     return nullptr;
+
+        return EntityIterator();
+    }
+
+    //EntityIterator Map::EndEntity()
+    //{
+    //    return EntityIterator();
+    //}
 }
 
--- source/code/intern/src/data/data.map.h
+++ source/code/intern/src/data/data.map.h
@@ -4,21 +4,55 @@
 #include "data.entityIterator.h"
 #include "data.sector.h"
 
+#include <list>
+
 #include "../core/core_aabb.h"
+
+using floatList = std::list<float>;
+using floatListIterator = floatList::iterator;
 
 namespace Data
 {
     class Map
     {
+        // TODO: This needs to be a singleton and rename it to map system
     public:
+        EntityIterator Begin();
+        EntityIterator Begin(EntityCategory::Enum category);
+        EntityIterator Begin(Core::AABB2Float& AABB);
+        EntityIterator Begin(Core::AABB2Float& AABB, EntityCategory::Enum category);
 
-        EntityIterator BeginEntity(Core::AABB2Float& AABB, EntityCategory::Enum category);
-        EntityIterator NextEntity(EntityIterator where, Core::AABB2Float& AABB, EntityCategory::Enum category);
+        EntityIterator End();
 
-        EntityIterator BeginEntity(Core::AABB2Float& AABB);
-        EntityIterator NextEntity(EntityIterator where, Core::AABB2Float& AABB);
+        EntityIterator& Next(EntityIterator& where);
+        EntityIterator& Next(EntityIterator& where, EntityCategory::Enum category);
+        EntityIterator& Next(EntityIterator& where, Core::AABB2Float& AABB);
+        EntityIterator& Next(EntityIterator& where, Core::AABB2Float& AABB, EntityCategory::Enum category);
 
-        EntityIterator EndEntity();
+        // The following ones are old and need to be removed
+        //EntityIterator NextEntity(EntityIterator where, Core::AABB2Float& AABB, EntityCategory::Enum category);
+
+        //EntityIterator BeginEntity(Core::AABB2Float& AABB);
+        //EntityIterator NextEntity(EntityIterator where, Core::AABB2Float& AABB);
+
+        //EntityIterator EndEntity();
+
+        // GetAll alle in map
+        // GetAllInAABB alle in bestimmten bereich
+        //   Zuerst alle Sektoren raussuchen (unten links anfangen und dann rechts und dann nach oben (Zeilenweise)
+        //   Durchlaeuft alle Folder und gibt alle Entities zurueck die im Bereich liegen
+        //   Falls keine gefunden werden in dem Sektor, dann muss in den naechsten Sektor gesprungen werden
+        //   Wenn wir eine Entity haben und bei wir die Category von der aktuellen Entity genommen und dann in dem Folder mit dem KategoryIndex++ der Folder wieder aufgerufen. Falls das alles leer ist nehmen wir den owner Sektor und gehen in den
+        // GetAllInAABBWithCategory alle in bestimmten bereich mit bestimmten typ
+        void GetExample(floatListIterator& begin, floatListIterator& end);
+        // or
+        std::pair<floatListIterator, floatListIterator> GetExample2();
+
+
+        // We need a AddEntity, RemoveEntity and MoveEntity
+        // AddEntity checks where the entity is on the map and then decides in which sector/folder it should be placed
+        // Remove is basically the same
+        // MoveEntity calculates the new position  and then has to check if the it switched in sector and if so we have to remove it from the old list and add it to the new one
 
     private:
 
